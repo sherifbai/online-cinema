4e88f5468dda17378c9e26337ec8f1a40607cced
Sherif <godness980@gmail.com>
2022-04-14 22:40:17 +0500

    fix

---
+++ src/movie/movie.error.constants.ts
@@ -0,0 +1,1 @@
+export const MOVIE_NOT_FOUND_ERROR = 'Movie does not found';
---
+++ src/user/errors.constants.ts
@@ -0,0 +1,2 @@
+export const USER_NOT_FOUND_ERROR = 'User does not found!';
+export const ALREADY_EXIST_EMAIL_ERROR = 'E-mail already exist!';
